25006da4d58ff0ad409a28efc83b52212b41e8a5
Paweł Borkowski <borekosp@gmail.com>
2019-11-25 06:45:10 +0100

    udpade  app,js

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const allTasks = [];
-    const selected = [];
+document.addEventListener("DOMContentLoaded", function(){
+	const selected = [];
+	const allTasks = [];
+	
+	const removeAllBtn = document.getElementById('removeAll');
+	const removeSelectBtn = document.getElementById('removeSelect');
+	const taskMsg = document.querySelector("#taskMsg");
+	const addTaskBtn = document.querySelector("#addTask");
+	const mainTable = document.getElementById('mainTable');
+	let count = 0
+	// let taskMsgDynamic = taskMsg.value;
+	const add = ()=> {
+		count++
+	const countCell = document.createElement("td");
+	const msgCell = document.createElement("td");
+	const taskMsg = document.querySelector("#taskMsg").value;
+	const newRow = document.createElement('tr');
+	msgCell.innerText = taskMsg;
+	countCell.innerText = count;
+	newRow.appendChild(countCell);
+	newRow.appendChild(msgCell);
+	
+	
+	
+		mainTable.appendChild(newRow);
+		console.log(taskMsg);
+		
+	};
+	addTaskBtn.addEventListener('click', add);
+	// taskMsg.addEventListener("keyup", ()=>{
+	// 	taskMsgDynamic = taskMsg.value;
+	// 	console.log(taskMsgDynamic);
+	// })
+	
+	const select = () => {
+		
+	};
+	const remove = () => {
+		
+	};
+	const removeAll = () => {
+		
+	};
+	const removeSelected = () => {
+		
+	};
 
-    const removeAllBtn = document.querySelector(`removeAll`);
-    const removeSelecteBtn = document.querySelector(`removeSelect`);
-    const taskMsg = document.querySelector(`#taskMsg`);
-    const addTaskBtn = document.querySelector("#addTask");
-    constMain = document.getElementById(`mainTable`);
-    let count = 0;
-
-    // let taskMsgDynamic = taskMsg.value;
-    const add = () => {
-        const taskMsg = document.querySelector("#taskMsg").value;
-        const newRow = document.createElement('tr');
-        mainTable.innetText = taskMsg;
-        mainTable.appendChild(newRow);
-        console.log(taskMsg);
-
-    };
-    addTaskBtn.addEventListener('click', add());
-    // taskMsg.addEventListener("keydown", ()=>{
-    // taskMsgDynamic= taskMsg.value;
-    // console.log(taskMsgDynamic)
-    // })
-
-
-    const remove = () => {
-
-    };
-    const removeAll = () => {
-
-    };
-    const removeSelevted = () => {
-
-    };
-    const select = () => {
-
-    };
 })
